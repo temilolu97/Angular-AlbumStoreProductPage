be290d1a671ec808b786aaee0322fa11609d8549
Temilolu A. Ojo <temiloluwaabiodunojo@gmail.com>
2019-10-28 14:19:49 +0100

    Using Interfaces to describe data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,3 +1,4 @@
+import { Album } from '../album';
 import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo:Album;
   constructor( private _productService: ProductService) { 
    
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
+import { Observable } from 'rxjs/Observable';
 import { Http , Response } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Album } from './album';
 import  'rxjs/add/operator/map';
 
 @Injectable({
@@ -9,8 +11,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json'
   constructor(private _http: Http) { }
 
-  getAlbum(id:number){
+  getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=>
-      response.json());
+      <Album>response.json());
   }
 }
