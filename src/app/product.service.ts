97cc75bd8849c4bf4843ffbbb4d459ba7ecb3f38
Temilolu A. Ojo <temiloluwaabiodunojo@gmail.com>
2019-10-28 13:49:05 +0100

    new update to step 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Http , Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/add/map/operator';
+import  'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
